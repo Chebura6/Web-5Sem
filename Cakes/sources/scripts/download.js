365b5960fa96b0895fc6b01e8117651dcfe0f21f
Артем Парфёнов <artemparfenov@MacBook-Air-Artem-5.local>
2023-12-20 20:35:07 +0300

    fix: promises

--- Cakes/sources/scripts/download.js
+++ Cakes/sources/scripts/download.js
@@ -1,32 +1,28 @@
 let len = 0;
+
+const url = `https://jsonplaceholder.typicode.com/posts`;
+
+let urls =[]
+for (let i = 1; i < 10; ++i) {
+    urls.push(url + `/` + i);
+}
+
 function buttonClicked(button) {
     button.style.display = 'none';
     let loaders = document.getElementsByClassName("loader");
-    console.log(loaders[0]);
     loaders[0].style.display = 'inline-block';
-    const countOfPosts = 100;
-    let promise = new Promise(async function(resolve, reject) {
-        let posts = [];
-        for (let i = 1; i < countOfPosts; ++i) {
-            let response = await fetch(`https://jsonplaceholder.typicode.com/posts/${i}`);
-            if (!response.ok) {
-                reject("error");
-            }
 
-            let post = await response.json();
-            posts.push(post);
-        }
-        loaders[0].style.display = 'none';
-        len = posts.length;
-        resolve(posts);
-    })
-
-
-    promise.then(
+    Promise.all(urls.map(url =>
+        fetch(url).then(resp => resp.json())
+            .catch(() => {
+                alert("Что-то пошло не так, попробуйте позже");
+            })
+    )).then(
         result => {
             console.log("success");
+            loaders[0].style.display = 'none';
             console.log(result);
-            for (let i = 0 ; i < len; ++i) {
+            for (let i = 0; i < result.length; ++i) {
                 const title = document.createElement('p');
                 const text = document.createElement('p');
                 title.innerHTML = `title: ${result[i].title}`;
@@ -34,9 +30,9 @@ function buttonClicked(button) {
                 document.querySelector('footer').appendChild(title);
                 document.querySelector('footer').appendChild(text);
             }
-        },
-        error => {
-            alert("Что-то пошло не так, попробуйте позже");
-        });
+        }
+    );
 }
+
+// lazy loading
 
